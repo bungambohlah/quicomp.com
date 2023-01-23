8953724dd1cadd0f74922a581fdc2a596b4324b2
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-01-23 17:06:24 +0700

    fix: :wrench: fix tailwind configuration wrong minWidth & maxWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,6 @@ module.exports = {
     content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
   },
   theme: {
-    minWidth: {
-      40: '10rem',
-      60: '15rem',
-      80: '20rem',
-      100: '25rem',
-    },
-    maxWidth: {
-      120: '30rem',
-      160: '40rem',
-      200: '50rem',
-    },
     extend: {
       inset: {
         100: '100%',
