4e6765328e5b38f45be50708f44fc9b14b959470
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-04-24 23:10:53 +0700

    feat: :lipstick: add some styles for service contents

--- src/components/domain/layouts/Service.tsx
+++ src/components/domain/layouts/Service.tsx
@@ -1,3 +1,5 @@
+import services from '~/constants/services';
+
 function Service() {
   return (
     <section id="services" className="py-24">
@@ -7,91 +9,16 @@ function Service() {
             Our Services
           </h2>
         </div>
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="300">
+        <div className="grid grid-cols-1 gap-4 align-baseline sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+          {services.map((service, idx) => (
+            <div key={`service-${idx + 1}`} className="flex flex-col gap-2">
               <div className="text-5xl icon">
-                <i className="lni lni-cog" />
+                <i className={`lni ${service.icon}`} />
               </div>
-              <div>
-                <h3 className="service-title">Web Development</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
+              <h3 className="flex-grow service-title min-h-12">{service.title}</h3>
+              <p className="text-[#8f9dac] flex-grow">{service.description}</p>
             </div>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="600">
-              <div className="text-5xl icon">
-                <i className="lni lni-bar-chart" />
-              </div>
-              <div>
-                <h3 className="service-title">Graphic Design</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="900">
-              <div className="text-5xl icon">
-                <i className="lni lni-briefcase" />
-              </div>
-              <div>
-                <h3 className="service-title">Business Branding</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="1200">
-              <div className="text-5xl icon">
-                <i className="lni lni-pencil-alt" />
-              </div>
-              <div>
-                <h3 className="service-title">Content Writing</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="1500">
-              <div className="text-5xl icon">
-                <i className="lni lni-mobile" />
-              </div>
-              <div>
-                <h3 className="service-title">App Development</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3">
-            <div className="m-4" data-aos="fade-right" data-aos-delay="1800">
-              <div className="text-5xl icon">
-                <i className="lni lni-layers" />
-              </div>
-              <div>
-                <h3 className="service-title">Digital Marketing</h3>
-                <p className="text-[#8f9dac]">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae
-                  quaerat quia incidunt laborum aspernatur...
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/constants/services.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: 'SERVICE LAPTOP / MACBOOK',
+    description:
+      'Perbaikan dan pemeliharaan laptop dan Macbook seperti perbaikan hardware dan software, upgrade, dan instalasi ulang sistem operasi.',
+    icon: 'lni-laptop',
+  },
+  {
+    title: 'SERVICE TV LED / MONITOR',
+    description:
+      'Perbaikan dan pemeliharaan TV LED/Monitor seperti perbaikan gambar atau suara, perbaikan kabel dan penggantian komponen elektronik',
+    icon: 'lni-display',
+  },
+  {
+    title: 'INSTALL SOFTWARE',
+    description:
+      'Instalasi dan konfigurasi software pada komputer dan laptop, termasuk sistem operasi, aplikasi dan program lainnya',
+    icon: 'lni-cloud-download',
+  },
+  {
+    title: 'SPARE PART REPLACEMENT',
+    description:
+      'Penggantian komponen atau spare part yang rusak pada laptop, Macbook, TV LED/Monitor, dan perangkat elektronik lainnya',
+    icon: 'lni-cogs',
+  },
+  {
+    title: 'JASA MICRO SOLDERING SEMUA DEVICE',
+    description:
+      'Perbaikan komponen mikro yang rusak pada semua perangkat elektronik seperti laptop, smartphone, tablet, dan perangkat lainnya dengan menggunakan teknik micro soldering',
+    icon: 'lni-briefcase-alt',
+  },
+];
