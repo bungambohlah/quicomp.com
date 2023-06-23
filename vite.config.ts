9a267d4dfcd6cf1b589204f8d23a632b644690d2
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-06-23 20:38:45 +0700

    refactor: :wrench: refactor configuration to use vite plugin instead of config manually

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
 import ssr from 'vite-plugin-ssr/plugin';
+import vercel from 'vite-plugin-vercel';
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 import postcss from './postcss.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA(), ssr()],
+  plugins: [react(), tsconfigPaths(), VitePWA(), ssr(), vercel(), vercelSsr()],
   css: { postcss },
 });
